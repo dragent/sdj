34e5a0f7962ac34d15e1a39c900bf69f03d6dc89
Alexis CAU <dragent@users.noreply.github.com>
2020-07-08 15:11:13 +0200

    Delete event.js

--- js/event.js
+++
@@ -1,9 +0,0 @@
-// Test pour enlever les sessions à la fermeture de la page
-
-$(document).ready(function()
-{
-      $(window).on('beforeunload', function () {
-        alert("bye2");
-        window.Location="../php/controller/deconnection.php"
-    });
-});
