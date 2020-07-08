0e44938971ba27da3a3b2941fe6c1faeb54044a0
Alexis CAU <dragent@users.noreply.github.com>
2020-07-08 15:13:20 +0200

    Delete includePhp.php

--- php/view/includePhp.php
+++
@@ -1,5 +0,0 @@
-<link href="../../css/verticalMenu.css" rel="stylesheet" type="text/css"/>
-<link href="../../css/page.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="../../js/jquery-3.4.1.js"></script>
-<script type="text/javascript" src="../../js/menuSwitch.js"></script>
-<script type="text/javascript" src="../../js/event.js"></script>
