ed76e7a77c60a2ffb1e7da5f47b85fafa4c1affb
Alexis CAU <dragent@users.noreply.github.com>
2020-09-27 10:12:20 +0200

    Add files via upload

--- src/view/templates/dataContainer.php
+++ src/view/templates/dataContainer.php
@@ -1,4 +1,4 @@
-<table table-responsive>
+<table class="table-responsive-md  table-striped table-wrapper-scroll-y my-custom-scrollbar">
     <thead>
         <tr>
             <th>Nom</th>
@@ -6,6 +6,7 @@
             <th>Téléphone</th>
             <th>Sur place</th>
             <th>A emporter</th>
+            <th>Blacklist</th>
             <th>Modifier</th>
         </tr>
     </thead>
@@ -13,18 +14,15 @@
         <?php
         for($i=0;$i<count($customerObject);$i++)
         {
+           $ban = ($customerObject[$i]->isBan()==1) ?  "Safe":  "Blacklist" ;
              ?>
-             <tr
-                <?php if($customerObject[$i]->isBan()==1)
-                {
-                    ?> class="banCust"<?php
-                }
-                ?>>
+             <tr>
                 <td><?=$customerObject[$i]->getName();?></td>
-                <td><?=  $customerObject[$i]->getNickname();?></td>
-                <td><?=  $customerObject[$i]->getPhone();?></td>
-                <td><?=  ($customerObject[$i]->isBooking()==1) ?  "Oui":  "Non" ;?></td>
-                <td><?=  ($customerObject[$i]->isTakeaway()==1) ?  "Oui":  "Non" ;?></td>
+                <td><?=$customerObject[$i]->getNickname();?></td>
+                <td><?=$customerObject[$i]->getPhone();?></td>
+                <td><?=($customerObject[$i]->isBooking()==1) ?  "Oui":  "Non" ;?></td>
+                <td><?=($customerObject[$i]->isTakeaway()==1) ?  "Oui":  "Non" ;?></td>
+                <td class="<?=$ban?>"><?=($customerObject[$i]->isBan()==1) ?  "Autorise":  "Blacklist" ;?></td>
                 <td>
                     <img id="modif" src="<?=  DIRECTORY_SEPARATOR."public".DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR."modification.png"; ?>"  onclick=activeModification("<?=$customerObject[$i]->getId()?>") alt="modification de la personne"/></a>
                 </td>
@@ -32,7 +30,6 @@
             <?php
          }
          ?>
- 
+
    </tbody>
 </table>
-	
